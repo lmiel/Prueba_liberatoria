45e9faa9c32819c8ea07d7fc365c2e24fd7c9e42
lmiel <lucia.mielgo@gmail.com>
2022-12-09 10:38:03 +0100

    fibonacci actualizado

--- FIBONACCI/fibonacci.py
+++ FIBONACCI/fibonacci.py
@@ -1,9 +1,18 @@
 m = 1000000
 
 def fibonacci(n):
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
-    
-print (fibonacci(m))
+    a = 0
+    b = 1
+    if n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2,n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+  
+print(fibonacci(m))
+
